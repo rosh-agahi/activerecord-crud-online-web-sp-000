d4041747fbc5a42e769ee884a5b3537d7a0970ec
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 00:30:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,7 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1)
+  Movie.find_by(id:1)
 end
 
 def can_find_by_multiple_attributes
@@ -56,7 +56,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title: "Title", release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
