b9878cbdd535850cf53a748bd3716dbd61b02e3f
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 00:25:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -20,10 +20,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
+  movie = Movie.create(attributes)
+    
 end
 
 def can_be_created_in_a_block(args = __)
