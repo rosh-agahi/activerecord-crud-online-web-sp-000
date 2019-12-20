271047c4c9a7f8dcc5a837354a8eeed0abbfe7f3
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 00:17:29 +0000

    Automatically backed up by Learn

--- db/migrate/20191220001541_create_movies.rb
+++ db/migrate/20191220001541_create_movies.rb
@@ -1,7 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.string :title,
-      t.datetime :
+      t.string :title
+      t.integer :release_date 
+      t.string :director 
+      t.string :lead 
+      t.boolean :in_theaters
   end
 end
